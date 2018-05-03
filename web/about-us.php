35a3d11c5108b2d72d35f8fcb9c82f1993fb742a
danielmartincraig <danielmartincraig@gmail.com>
2018-05-03 16:16:10 -0600

    added navbar

--- web/about-us.php
+++ web/about-us.php
@@ -5,9 +5,11 @@
     <title>Title</title>
 </head>
 <body>
-<div>
-    <h1>Rexburg Iphone Repair</h1>
-</div>
+
+<?php
+    include("navbar.php");
+?>
+
 <div>
     <p>Dropped your phone?  Bring it to us!</p>
 </div>
---
+++ web/navbar.php
@@ -0,0 +1,7 @@
+<header>
+    <h1>Rexburg Iphone Repair</h1>
+    <br>
+    <button>Home</button>
+    <button>About Us</button>
+    <button>Login</button>
+</header>
