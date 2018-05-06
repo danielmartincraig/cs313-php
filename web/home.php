e638be571e562fa3e27cc100cc1c3170bde00875
danielmartincraig <danielmartincraig@gmail.com>
2018-05-05 18:53:25 -0600

    added content on homepage

--- web/home.php
+++ web/home.php
@@ -12,13 +12,24 @@
     <title>Title</title>
 </head>
 <body>
-<div>
+<div id="banner">
     <h1>Welcome to my homepage!</h1>
-</div>
-<div>
     <h3>My homepage is focused on my interest in Computational Linguistics</h3>
     <p>Although I don't know everything about it yet, I'll do my best to make this page informative and useful to those
         who are interested in Computational Linguistics like I am!</p>
+</div>
+<div id="sidebar">
+    <h1>Things that I love or that I want to learn</h1>
+    <ul>
+        <li><a href="https://www.amazon.com/Computational-Approaches-Morphology-Syntax-Surveys/dp/0199274789/">My current textbook</a></li>
+        <li><a href="https://www.scottaaronson.com/blog/">Scott Aaronson's blog</a></li>
+        <li><a href="https://tomassetti.me/antlr-mega-tutorial/">Antlr Mega Tutorial</a></li>
+        <li><a href="https://xkcd.com/">XKCD</a></li>
+        <li><a href="https://www.python.org/">Python 3</a></li>
+        <li><a href="http://www.byui.edu/">My University</a></li>
+    </ul>
+</div>
+<div id="maintext">
 </div>
 </body>
 </html>
