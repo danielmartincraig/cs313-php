aba4aac87acfca3bbf50ee7d1654ea2b73448a70
danielmartincraig <danielmartincraig@gmail.com>
2018-05-05 18:41:50 -0600

    added homepage

---
+++ web/home.php
@@ -0,0 +1,24 @@
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 5/5/18
+ * Time: 6:36 PM
+ */
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div>
+    <h1>Welcome to my homepage!</h1>
+</div>
+<div>
+    <h3>My homepage is focused on my interest in Computational Linguistics</h3>
+    <p>Although I don't know everything about it yet, I'll do my best to make this page informative and useful to those
+        who are interested in Computational Linguistics like I am!</p>
+</div>
+</body>
+</html>
