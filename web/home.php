06e1a1e99d3dc44ab0418982e14b97f080da0190
danielmartincraig <danielmartincraig@gmail.com>
2018-05-05 19:06:52 -0600

    added outer div

--- web/home.php
+++ web/home.php
@@ -14,24 +14,26 @@
     <link rel="stylesheet" href="resources/userstyle.css">
 </head>
 <body>
-<div id="banner">
-    <h1>Welcome to my homepage!</h1>
-    <h3>My homepage is focused on my interest in Computational Linguistics</h3>
-    <p>Although I don't know everything about it yet, I'll do my best to make this page informative and useful to those
-        who are interested in Computational Linguistics like I am!</p>
-</div>
-<div id="sidebar">
-    <h1>Things that I love or that I want to learn</h1>
-    <ul>
-        <li><a href="https://www.amazon.com/Computational-Approaches-Morphology-Syntax-Surveys/dp/0199274789/">My current textbook</a></li>
-        <li><a href="https://www.scottaaronson.com/blog/">Scott Aaronson's blog</a></li>
-        <li><a href="https://tomassetti.me/antlr-mega-tutorial/">Antlr Mega Tutorial</a></li>
-        <li><a href="https://xkcd.com/">XKCD</a></li>
-        <li><a href="https://www.python.org/">Python 3</a></li>
-        <li><a href="http://www.byui.edu/">My University</a></li>
-    </ul>
-</div>
-<div id="maintext">
+<div>
+    <div id="banner">
+        <h1>Welcome to my homepage!</h1>
+        <h3>My homepage is focused on my interest in Computational Linguistics</h3>
+        <p>Although I don't know everything about it yet, I'll do my best to make this page informative and useful to those
+            who are interested in Computational Linguistics like I am!</p>
+    </div>
+    <div id="sidebar">
+        <h1>Things that I love or that I want to learn</h1>
+        <ul>
+            <li><a href="https://www.amazon.com/Computational-Approaches-Morphology-Syntax-Surveys/dp/0199274789/">My current textbook</a></li>
+            <li><a href="https://www.scottaaronson.com/blog/">Scott Aaronson's blog</a></li>
+            <li><a href="https://tomassetti.me/antlr-mega-tutorial/">Antlr Mega Tutorial</a></li>
+            <li><a href="https://xkcd.com/">XKCD</a></li>
+            <li><a href="https://www.python.org/">Python 3</a></li>
+            <li><a href="http://www.byui.edu/">My University</a></li>
+        </ul>
+    </div>
+    <div id="maintext">
+    </div>
 </div>
 </body>
 </html>
