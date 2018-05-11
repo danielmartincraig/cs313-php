b7c384e46715cb329d1a8ebff4ce4bbb093195b2
danielmartincraig <danielmartincraig@gmail.com>
2018-05-10 21:05:17 -0600

    added item details

--- web/assignments/prove03/storefront.php
+++ web/assignments/prove03/storefront.php
@@ -6,9 +6,9 @@
  * Time: 8:25 PM
  */
 
-$item1 = array('brand' => '', 'name' => '', 'description' => '', 'price' => '');
-$item2 = array('brand' => '', 'name' => '', 'description' => '', 'price' => '');
-$item3 = array('brand' => '', 'name' => '', 'description' => '', 'price' => '');
+$item1 = array('brand' => 'Yeti', 'name' => 'Super Cooler', 'description' => 'Perfect for tubing down the river!', 'price' => '$300.00');
+$item2 = array('brand' => 'Parker Brothers', 'name' => 'Monopoly', 'description' => 'Family Fun for Everyone!', 'price' => '$15.00');
+$item3 = array('brand' => 'Meow Mix', 'name' => 'Cat Chow', 'description' => 'Keeps your kitty healthy!', 'price' => '$8.00');
 
 items = array($item1, $item2, $item3);
 
