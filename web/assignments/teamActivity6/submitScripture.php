9f4bcc45f343fdd47d0cba3270ed2937cbe314e8
danielmartincraig <danielmartincraig@gmail.com>
2018-05-29 16:50:57 -0600

    added code to create scripture_topic rows

--- web/assignments/teamActivity6/submitScripture.php
+++ web/assignments/teamActivity6/submitScripture.php
@@ -32,15 +32,27 @@ $book = $_POST['book'];
 $chapter = $_POST['chapter'];
 $verse = $_POST['verse'];
 $content = $_POST['content'];
+$topic_array = $_POST['formTopic'];
 
 $topic_name_stmt = $pdo->prepare("SELECT name FROM topic");
 $topic_name_stmt->execute();
 
-$topics = $topic_name_stmt->fetchall(PDO::FETCH_ASSOC);
-print_r($topics);
-
 $submit_scripture_stmt = $pdo->prepare("INSERT INTO scripture (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content);");
-$submit_scripture_stmt->execute(array(":book" => $book, ":chapter" => $chapter, ":verse" => $verse, ":content" => $content));
+$submit_scripture_stmt->execute(array(':book' => $book, ':chapter' => $chapter, ':verse' => $verse, ':content' => $content));
+$last_scripture_id = $pdo->lastInsertId('scripture_id_seq');
+
+foreach ($topic_array as $topic) {
+    $get_topic_id_stmt = $pdo->prepare("(SELECT id FROM topic WHERE name = :topic_name);");
+    $get_topic_id_stmt->bindValue(':topic_name', $topic, PDO::PARAM_STR);
+    $get_topic_id_stmt->execute();
+    $topic_id = $get_topic_id_stmt->fetchall(PDO::FETCH_ASSOC)['id'];
+
+    $submit_scripture_topic_stmt = $pdo->prepare("INSERT INTO scripture_topic (scripture_id, topic_id) VALUES (:scripture_id, :topic_id)");
+    $topic_name_stmt->bindValue(':scripture_id', $last_scripture_id, PDO::PARAM_INT);
+    $topic_name_stmt->bindValue(':topic_id', $topic_id, PDO::PARAM_INT);
+    $topic_name_stmt->execute();
+}
+
 
 ?>
 
