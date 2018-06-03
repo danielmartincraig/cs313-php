cfe3deb46f5b091795a2e9a240e129aecd6acc8d
danielmartincraig <danielmartincraig@gmail.com>
2018-06-03 00:40:06 -0600

    moved pdo get into seperate file

--- web/assignments/TreeStyleNotes/treeStyleNotes.php
+++ web/assignments/TreeStyleNotes/treeStyleNotes.php
@@ -7,27 +7,9 @@
  * TreeStyleNotes web app
  ***********************************************************/
 
-try
-{
-    $db = parse_url(getenv("DATABASE_URL"));
-    $pdo = new PDO("pgsql:" . sprintf(
-            "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-            $db["host"],
-            $db["port"],
-            $db["user"],
-            $db["pass"],
-            ltrim($db["path"], "/")
-        ));
-    // this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
-    $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch (PDOException $ex)
-{
-    // If this were in production, you would not want to echo
-    // the details of the exception.
-    echo "Error connecting to DB. Details: $ex";
-    die();
-}
+require("getDb.php");
+
+$pdo = getDbConnection();
 
 function showChildren($pdo, $root)
 {
